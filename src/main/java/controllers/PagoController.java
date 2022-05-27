e2dc146e6c37c839920ca4e92ea2b5bc60bd4e9e
asantoro36 <axelsantoro@hotmail.com>
2022-05-27 16:51:59 -0300

    Agrega atributos de OrdenDeCompra

--- src/main/java/controllers/PagoController.java
+++ src/main/java/controllers/PagoController.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import model.entidades.Pago;
-
 public class PagoController {
 
     private static PagoController controller;
--- src/main/java/model/entidades/OrdenDeCompra.java
+++ src/main/java/model/entidades/OrdenDeCompra.java
@@ -2,11 +2,20 @@ package model.entidades;
 
 import model.dtos.DatosIvaDTO;
 
+import java.util.Date;
+import java.util.List;
+
 public class OrdenDeCompra {
 
     private Proveedor proveedor;
+    private List<ItemOrdenCompra> items;
+    private Documento documento;
+    private Integer cantidad;
+    private Double precioAcordado;
+    private Date fecha;
 
     public OrdenDeCompra(Proveedor proveedor) {
+
         this.proveedor = proveedor;
     }
 
